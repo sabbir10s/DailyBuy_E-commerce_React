bbc52881d4456cb1e48f687674d4971fb3fb6ea9
alamincoders <developeralamin03@gmail.com>
2023-02-18 19:07:37 +0600

    cart page added

--- src/components/screen/ProductCart.jsx
+++ src/components/screen/ProductCart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MenuBar from "../shared/MenuBar";
 import ProcessingSteps from "./ProcessingSteps";
+import cartImg from "../../assets/products/product.png";
+import { Link } from "react-router-dom";
 
 const ProductCart = () => {
   return (
@@ -12,44 +14,111 @@ const ProductCart = () => {
           <div className="lg:col-span-3  px-3 shadow-custom bg-[#F9FAFB] rounded-lg">
             <div className="grid grid-cols-7 gap-4">
               <h1 className=" col-span-3  font-semibold py-5 uppercase">Product</h1>
-              <h1 className="font-semibold py-5 uppercase ">Unite Price</h1>
-              <h1 className="font-semibold py-5 uppercase ">Quantity</h1>
-              <h1 className="font-semibold py-5 uppercase ">Total</h1>
-              <h1 className="font-semibold py-5 uppercase ">Remove</h1>
+              <h1 className="font-semibold py-5 uppercase text-center">Unite Price</h1>
+              <h1 className="font-semibold py-5 uppercase text-center">Quantity</h1>
+              <h1 className="font-semibold py-5 uppercase text-center">Total</h1>
+              <h1 className="font-semibold py-5 uppercase text-center">Remove</h1>
             </div>
             {/* every product  cart details*/}
-            <div className="grid grid-cols-7 gap-4  bg-white py-3 mb-3 rounded">
-              <div className="col-span-3 ">
-                <div className="flex space-x-2 items-center">
-                  <div>
-                    <img src="" alt="product-img" />
+            {Array.from({ length: 2 }).map((_, idx) => (
+              <div className="grid grid-cols-7 gap-4  bg-white p-3 mb-3 rounded items-center">
+                <div className="col-span-3 ">
+                  <div className="flex space-x-2 items-center">
+                    <div className="w-[120px] h-[120px] bg-gray-50  rounded-2xl">
+                      <img src={cartImg} className="w-full h-full object-center object-fill" alt="product-img" />
+                    </div>
+                    <p className="font-semibold text-base">Nestle Original Coffee-Mate Coffee Creamer</p>
                   </div>
-                  <p className="font-semibold text-base">Nestle Original Coffee-Mate Coffee Creamer</p>
                 </div>
-              </div>
-              <div>
-                <p className="text-gray-600 pl-6">$10.00</p>
-              </div>
-              <div>
-              <div className="flex items-center border border-gray-50 p-1 rounded-lg">
+                <div>
+                  <p className="text-gray-600 text-center">$10.00</p>
+                </div>
+                <div>
+                  <div className="flex items-center justify-center">
                     <button type="button" className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75">
                       &minus;
                     </button>
 
-                    <span className="px-6 font-semibold">1</span>
+                    <span className="px-4 font-semibold">1</span>
 
                     <button type="button" className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75">
                       &#43;
                     </button>
                   </div>
+                </div>
+                <div>
+                  <p className="text-primary-600 font-semibold text-center pl-4">$20.00</p>
+                </div>
+                <div className="text-center">
+                  <button className="">
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 text-gray-500 inline-block hover:text-primary-600"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-              <div>4</div>
-              <div>5</div>
+            ))}
+          </div>
+          <div className="relative">
+            <div className="lg:absolute space-y-5 top-0 left-0 w-full h-auto  py-3 px-4 shadow-custom border rounded-lg">
+              <div className="flex justify-between items-center">
+                <p className="text-gray-600 text-base">Subtotal:</p>
+                <p className="text-gray-700 font-semibold text-xl">$40.00</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="text-gray-600 text-base">Tax:</p>
+                <p className="text-gray-700 font-semibold text-xl">$0.00</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="text-gray-600 text-base">Shipping:</p>
+                <p className="text-gray-700 font-semibold text-xl">$3.00</p>
+              </div>
+              <hr className="" />
+              <div className="flex justify-between items-center">
+                <p className="text-black font-medium text-base">Total:</p>
+                <p className="text-primary-600 font-bold text-xl">$43.00</p>
+              </div>
+              <button className="px-5 w-full py-2.5 relative rounded group font-medium text-white  inline-block">
+                {" "}
+                <Link to="/checkout">
+                  {" "}
+                  <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-primary-600 to-primary-500"></span>
+                  <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-primary-600 to-primary-500"></span>
+                  <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-primary-600 to-primary-500"></span>
+                  <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-primary-600 from-primary-500"></span>
+                  <span className="relative">Check out</span>
+                </Link>{" "}
+              </button>
             </div>
           </div>
-          <div className=" p-3 shadow-custom rounded-lg"></div>
+        </div>
+        {/* shipping button */}
+        <div className="mt-14">
+          {" "}
+          <button className="px-5  py-2.5 relative rounded group font-medium text-white  inline-block">
+            <Link to="/categories">
+              <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-primary-600 to-primary-500"></span>
+              <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-primary-600 to-primary-500"></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-primary-600 to-primary-500"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-primary-600 from-primary-500"></span>
+              <span className="relative">Continue Shipping</span>
+            </Link>
+          </button>
         </div>
       </div>
+
       <ProcessingSteps />
     </>
   );
