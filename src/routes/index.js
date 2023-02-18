a8b2455cb8f6ecf58e3fe33cda78db4eb7bf33e2
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-18 15:54:43 +0600

    blog page added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import AboutUs from "../pages/AboutUs";
 import AboutUsV2 from "../pages/AboutUsV2";
+import BlogPage from "../pages/BlogPage";
 import ContactUs from "../pages/ContactUs";
 import FAQ from "../pages/FAQ";
 const Categories = lazy(() => import("../pages/Categories"));
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: "/product/:id",
         element: <ProductDetails />,
+      },
+      {
+        path: "/blog",
+        element: <BlogPage />,
       },
       {
         path: "/about",
