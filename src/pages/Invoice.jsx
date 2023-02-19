8594bcc0a77d50c769cfcb64b1cd83bf198fee83
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-19 15:37:32 +0600

    invoice page added

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import InvoiceBanner from '../components/screen/Invoice/InvoiceBanner';
+import InvoiceTable from '../components/screen/Invoice/InvoiceTable';
 
 const Invoice = () => {
     return (
         <>
             <InvoiceBanner/>
+            <InvoiceTable/>
         </>
     );
 };
