effebbee9c271d2d1bf1760e5aab00e7ac497ed8
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-20 14:34:50 +0600

    category page responsive done

--- src/components/screen/Categories/CategoriesProduct.jsx
+++ src/components/screen/Categories/CategoriesProduct.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Products from "../../shared/Products";
+import { CiFilter } from 'react-icons/ci';
+import { HiOutlineChartBar } from 'react-icons/hi';
 
 const CategoriesProduct = () => {
   return (
@@ -31,48 +33,18 @@ const CategoriesProduct = () => {
         </div>
       </div>
       {/* button */}
-      <div className=" flex justify-around items-center lg:hidden border-b border-gray-100 ">
-        <button className="py-3 w-full border-r focus:bg-primary-600 focus:text-white text-xl">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5 inline-block mr-2 -mt-1"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
-              />
-            </svg>
-          </span>
-          <span>Filter</span>
+      <div className=" grid grid-cols-2 text-[16px] lg:hidden">
+        <button className=" flex items-center gap-1 justify-center py-3 w-full border-r focus:bg-primary-600 focus:text-white text-xl">
+          <CiFilter className="text-gray-400 text-2xl"/>
+          <span className="text-gray-900">Filter</span>
         </button>
-        <button className="py-3 w-full   focus:bg-primary-600 focus:text-white text-xl">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5 inline-block mr-2 -mt-1"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-              />
-            </svg>
-          </span>
-          <span>Sort</span>
+        <button className="flex items-center justify-center gap-1 py-3 w-full   focus:bg-primary-600 focus:text-white text-xl">
+          <HiOutlineChartBar className="text-gray-400 text-2xl"/>
+          <span className="text-gray-900">Sort</span>
         </button>
       </div>
       {/* products */}
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[10px] md:gap-[20px] mt-8">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-[10px] md:gap-[20px] mt-8">
         {Array.from({ length: 15 }).map((_, idx) => (
           <Products key={idx} />
         ))}
