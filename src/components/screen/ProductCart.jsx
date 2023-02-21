773cadb940c09bbd293ebfcaa7e363a3f7ec772a
alamincoders <developeralamin03@gmail.com>
2023-02-21 09:34:12 +0600

    cart updated

--- src/components/screen/ProductCart.jsx
+++ src/components/screen/ProductCart.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import MenuBar from "../shared/MenuBar";
 import ProcessingSteps from "./ProcessingSteps";
-import cartImg from "../../assets/products/product.png";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, decreaseCart, removeFromCart } from "../../redux/feature/cartSlice";
 
 const ProductCart = () => {
-  const name = "Nestle Original Coffee-Mate Coffee Creamer";
   const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const handleRemoveItem = (cartItem) => {
+    dispatch(removeFromCart(cartItem));
+  };
+  const handleDecreaseCart = (cartItem) => {
+    dispatch(decreaseCart(cartItem));
+  };
+  const handleIncreaseCart = (cartItem) => {
+    dispatch(addToCart(cartItem));
+  };
 
   const total = cartItems.reduce((total, item) => total + item.price, 0);
   const shipping = 3;
@@ -19,62 +28,70 @@ const ProductCart = () => {
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 mt-8">
           <div className="lg:col-span-3  lg:px-3 lg:shadow-custom lg:bg-[#F9FAFB] lg:rounded-lg">
             {/* for mobile device */}
-            {cartItems.map((item, idx) => (
-              <div key={idx} className="grid grid-cols-3 lg:hidden gap-2 mt-3 items-center border-b border-gray-100 pb-2">
-                <div className="flex space-x-2 items-center col-span-2">
-                  <div className="w-[80px] h-[80px] bg-gray-50  rounded-2xl">
-                    <img src={item?.main_img_url} className="w-full h-full object-center object-fill" alt="product-img" />
-                  </div>
-                  <div>
-                    <p className="font-semibold text-sm">{item?.product_name.slice(0, 18)}...</p>
-                    <p className="text-primary-600  font-semibold">${item?.price}.00</p>
-                    <div className="flex items-center justify-between">
-                      <p className="text-gray-600">
-                        <del>${item?.del_price}.00</del>
-                      </p>
-                      <div className="text-end">
-                        <button className="">
-                          {" "}
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-4 h-4 inline-block text-secondary-600"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
-                        </button>
+            {cartItems.length > 0 ? (
+              cartItems.map((item, idx) => (
+                <div key={idx} className=" grid grid-cols-3 lg:hidden gap-2 mt-3 items-center border-b border-gray-100 pb-2">
+                  <div className="flex space-x-2 items-center col-span-2">
+                    <div className="w-[80px] h-[80px] bg-gray-50  rounded-2xl">
+                      <img src={item?.main_img_url} className="w-full h-full object-center object-fill" alt="product-img" />
+                    </div>
+                    <div>
+                      <p className="font-semibold text-sm">{item?.product_name.slice(0, 18)}...</p>
+                      <p className="text-primary-600  font-semibold">${item?.price}.00</p>
+                      <div className="flex items-center justify-between">
+                        <p className="text-gray-600">
+                          <del>${item?.del_price}.00</del>
+                        </p>
+                        <div className="text-end">
+                          <button onClick={() => handleRemoveItem(item)} className="">
+                            {" "}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-4 h-4 inline-block text-secondary-600"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              />
+                            </svg>
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div>
-                  <div className="flex items-center justify-center">
-                    <button
-                      type="button"
-                      className="w-8 h-8 text-xl font-semibold border border-primary-600  bg-gray-50 rounded-[50%] text-primary-600 transition hover:opacity-75"
-                    >
-                      &minus;
-                    </button>
+                  <div>
+                    <div className="flex items-center justify-center">
+                      <button
+                        onClick={() => handleDecreaseCart(item)}
+                        type="button"
+                        className="w-8 h-8 text-xl font-semibold border border-primary-600  bg-gray-50 rounded-[50%] text-primary-600 transition hover:opacity-75"
+                      >
+                        &minus;
+                      </button>
 
-                    <span className="px-4 text-xl">1</span>
+                      <span className="px-4 text-xl">{item?.cartQuantity}</span>
 
-                    <button
-                      type="button"
-                      className="w-8 h-8 text-xl font-semibold border border-primary-600  bg-gray-50 rounded-[50%] text-primary-600 transition hover:opacity-75"
-                    >
-                      &#43;
-                    </button>
+                      <button
+                        onClick={() => handleIncreaseCart(item)}
+                        type="button"
+                        className="w-8 h-8 text-xl font-semibold border border-primary-600  bg-gray-50 rounded-[50%] text-primary-600 transition hover:opacity-75"
+                      >
+                        &#43;
+                      </button>
+                    </div>
                   </div>
                 </div>
+              ))
+            ) : (
+              <div className=" py-5 block lg:hidden text-3xl text-center text-primary-600 font-semibold">
+                <p>Your Cart is Empty</p>
               </div>
-            ))}
+            )}
             <div className="hidden lg:grid grid-cols-7 gap-4">
               <h1 className=" col-span-3  font-semibold py-5 uppercase">Product</h1>
               <h1 className="font-semibold py-5 uppercase text-center">Unite Price</h1>
@@ -83,58 +100,72 @@ const ProductCart = () => {
               <h1 className="font-semibold py-5 uppercase text-center">Remove</h1>
             </div>
             {/* every product  cart details*/}
-            {cartItems.map((item, idx) => (
-              <div key={idx} className="hidden lg:grid grid-cols-7 gap-4  bg-white p-3 mb-3 rounded items-center">
-                <div className="col-span-3 ">
-                  <div className="flex space-x-2 items-center">
-                    <div className="w-[120px] h-[120px] bg-gray-50  rounded-2xl">
-                      <img src={item?.main_img_url} className="w-full h-full object-center object-fill" alt="product-img" />
+            {cartItems.length > 0 ? (
+              cartItems.map((item, idx) => (
+                <div key={idx} className="hidden lg:grid grid-cols-7 gap-4  bg-white p-3 mb-3 rounded items-center">
+                  <div className="col-span-3 ">
+                    <div className="flex space-x-2 items-center">
+                      <div className="w-[120px] h-[120px] bg-gray-50  rounded-2xl">
+                        <img src={item?.main_img_url} className="w-full h-full object-center object-fill" alt="product-img" />
+                      </div>
+                      <p className="font-semibold text-base">{item?.product_name}</p>
                     </div>
-                    <p className="font-semibold text-base">{item?.product_name}</p>
+                  </div>
+                  <div>
+                    <p className="text-primary-600 font-semibold text-center">${item?.price}.00</p>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-center">
+                      <button
+                        onClick={() => handleDecreaseCart(item)}
+                        type="button"
+                        className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75"
+                      >
+                        &minus;
+                      </button>
+
+                      <span className="px-4 font-semibold">{item.cartQuantity}</span>
+
+                      <button
+                        onClick={() => handleIncreaseCart(item)}
+                        type="button"
+                        className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75"
+                      >
+                        &#43;
+                      </button>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-gray-600 text-center pl-4">
+                      <del>${item?.del_price}.00</del>
+                    </p>
+                  </div>
+                  <div className="text-center">
+                    <button onClick={() => handleRemoveItem(item)} className="">
+                      {" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-gray-500 inline-block hover:text-primary-600"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
                   </div>
                 </div>
-                <div>
-                  <p className="text-primary-600 font-semibold text-center">${item?.price}.00</p>
-                </div>
-                <div>
-                  <div className="flex items-center justify-center">
-                    <button type="button" className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75">
-                      &minus;
-                    </button>
-
-                    <span className="px-4 font-semibold">1</span>
-
-                    <button type="button" className="w-10 h-10 text-xl bg-gray-50 rounded-[50%] leading-10 text-gray-600 transition hover:opacity-75">
-                      &#43;
-                    </button>
-                  </div>
-                </div>
-                <div>
-                  <p className="text-gray-600 text-center pl-4">
-                    <del>${item?.del_price}.00</del>
-                  </p>
-                </div>
-                <div className="text-center">
-                  <button className="">
-                    {" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6 text-gray-500 inline-block hover:text-primary-600"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </div>
+              ))
+            ) : (
+              <div className=" py-5 hidden lg:block text-3xl text-center text-primary-600 font-semibold">
+                <p>Your Cart is Empty</p>
               </div>
-            ))}
+            )}
           </div>
           {/*  */}
           <div className="relative">
