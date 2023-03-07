1fa17b809ccb644347f3b0c90f8ac1e959c8dd66
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-07 19:18:46 +0600

    modal added

--- src/components/shared/TopSearchBar.jsx
+++ src/components/shared/TopSearchBar.jsx
@@ -1,65 +1,51 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 import logo from "../../assets/logo/big_logo.png";
+import Modal from "./Modal";
 
 const TopSearchBar = () => {
-  const { cartItems } = useSelector((state) => state.cart);
+  const {cartItems} = useSelector((state) => state.cart);
+  const [showModal,
+    setShowModal] = useState(false)
+
+  const handleOnClose = () => {
+    setShowModal(false)
+  }
   return (
-    <div className="hidden lg:block border border-gray-100/60">
-      <div className="container ">
-        <div className="flex justify-between py-5 items-center">
-          <Link to="/">
-            <div className="cursor-pointer w-[200px] h-14">
-              <img src={logo} className="w-full h-full" alt="" />
-            </div>
-          </Link>
-          <div className=" lg:w-[550px] xl:w-[778px] relative">
-            <input
-              placeholder="I'm looking for..."
-              type="text"
-              className="py-3 px-5 border border-gray-200/80 w-[92%] focus:outline-primary-600 rounded-l-md bg-gray-50"
-            />
-            <button className="bg-primary-600 text-white p-3 w-[8%] h-full rounded-r-md absolute top-0 right-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 inline-block"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
-            </button>
-          </div>
-          <div className="space-x-8 flex items-center">
-            <Link className="txtHover text-base" to="/cart">
-              <div className="bg-secondary-50 inline-block w-14 h-14 rounded-[50%] relative">
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 inline-block text-secondary-600 absolute top-4 left-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    />
-                  </svg>
-                </span>
-                <span className="w-5 h-5 text-sm inline-block rounded-[50%] bg-secondary-600 text-white text-center font-semibold absolute top-2  right-1">
-                  {cartItems.length}
-                </span>
+    <div>
+      <div className="hidden lg:block border border-gray-100/60">
+        <div className="container ">
+          <div className="flex justify-between py-5 items-center">
+            <Link to="/">
+              <div className="cursor-pointer w-[200px] h-14">
+                <img src={logo} className="w-full h-full" alt=""/>
               </div>
-            </Link>{" "}
-            <Link className="txtHover text-base" to="/signIn">
-              <div className="flex items-center space-x-2">
-                <div className="bg-[#F8F8F8]  w-12 h-12 rounded-[50%] relative">
+            </Link>
+            <div className=" lg:w-[550px] xl:w-[778px] relative">
+              <input
+                placeholder="I'm looking for..."
+                type="text"
+                className="py-3 px-5 border border-gray-200/80 w-[92%] focus:outline-primary-600 rounded-l-md bg-gray-50"/>
+              <button
+                className="bg-primary-600 text-white p-3 w-[8%] h-full rounded-r-md absolute top-0 right-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 inline-block">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                </svg>
+              </button>
+            </div>
+            <div className="space-x-8 flex items-center">
+              <Link className="txtHover text-base" to="/cart">
+                <div className="bg-secondary-50 inline-block w-14 h-14 rounded-[50%] relative">
                   <span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -67,25 +53,48 @@ const TopSearchBar = () => {
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="w-6 h-6 inline-block absolute top-3 left-3"
-                    >
+                      className="w-6 h-6 inline-block text-secondary-600 absolute top-4 left-4">
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                      />
+                        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
                     </svg>
                   </span>
+                  <span
+                    className="w-5 h-5 text-sm inline-block rounded-[50%] bg-secondary-600 text-white text-center font-semibold absolute top-2  right-1">
+                    {cartItems.length}
+                  </span>
                 </div>
-                <div className=" leading-4">
-                  <span className="text-gray-400">Hello</span>
-                  <p className="text-base">Sign in</p>
+              </Link>{" "}
+              <button onClick={() => setShowModal(true)}>
+                <div className="flex items-center space-x-2">
+                  <div className="bg-[#F8F8F8]  w-12 h-12 rounded-[50%] relative">
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 inline-block absolute top-3 left-3">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
+                      </svg>
+                    </span>
+                  </div>
+                  <div className=" leading-4">
+                    <span className="text-gray-400">Hello</span>
+                    <p className="text-base">Sign in</p>
+                  </div>
                 </div>
-              </div>
-            </Link>{" "}
+              </button>{" "}
+            </div>
           </div>
         </div>
       </div>
+        <Modal  onClose={handleOnClose} visible={showModal}/>
     </div>
   );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,59 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import error from "../assets/auth/login.png";
 import Checkbox from "../components/theme/Checkbox";
-
-const SignUp = () => {
+import '.././styles/SignUp.css'
+const SignUp = ({setPath}) => {
   return (
-    <div className="pt-20 px-3 lg:mb-10">
-      <div className="lg:w-[970px] lg:h-[600px] mx-auto rounded-lg shadow-custom grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
-        <div className="w-full h-full">
-          <img className="w-full h-full rounded-t-lg lg:rounded-l-lg" src={error} alt="" />
+    <div className="">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-items-center w-full">
+        <div className=" w-[400px] lg:w-[470px] h-full hidden md:block signUpBg">
         </div>
-        <div className="pr-3">
+        <div className="w-full p-4">
           <h2 className="text-2xl font-semibold text-center mb-10">SignUp</h2>
-          <form onSubmit={(e) => e.preventDefault()} action="#" className="space-y-5 pl-2 lg:pl-0 lg:px-2">
+          <form onSubmit={(e) => e.preventDefault()} action="#" className="space-y-5">
             <div className="text-center">
               <input
-                className="px-4 py-3  rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
+                className="px-4 py-3 rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
                 type="text"
                 placeholder="Full Name"
               />
             </div>{" "}
             <div className="text-center">
               <input
-                className="px-4 py-3  rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
+                className="px-4 py-3 rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
                 type="email"
                 placeholder="Email"
               />
             </div>
             <div className=" text-center">
               <input
-                className="px-4 py-3  rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
+                className="px-4 py-3 rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
                 type="password"
                 placeholder="Password"
               />
             </div>{" "}
             <div className=" text-center">
               <input
-                className="px-4 py-3  rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
+                className="px-4 py-3 rounded-lg w-full focus:border-primary-600 outline-none border lg:w-5/6"
                 type="password"
                 placeholder="Confirm Password"
               />
             </div>{" "}
-            <p className="flex items-center lg:w-5/6 mx-auto">
+            <div className="flex items-center lg:w-5/6 mx-auto">
               <span className="-mt-2">
                 {" "}
                 <Checkbox />
               </span>
-              <span>
-                I agree to{" "}
-                <Link to="/terms">
-                  <b className="text-primary-600 hover:underline cursor-pointer">terms and condition</b>
-                </Link>
-              </span>
-            </p>
-            <div className="text-center pb-10">
+              <div className="flex items-center gap-1">
+                <p>I agree to{" "}</p>
+                <button onClick={()=>setPath('terms')} className="text-primary-600 font-bold hover:underline cursor-pointer">
+                  terms and condition
+                </button>
+              </div>
+            </div>
+            <div className="text-center">
               <button type="submit" className="px-5  w-full text-xl lg:w-5/6 py-2.5 relative rounded group font-medium text-white inline-block">
                 <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-primary-600 to-primary-500"></span>
                 <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-primary-600 to-primary-500"></span>
@@ -61,12 +58,12 @@ const SignUp = () => {
                 <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-primary-600 from-primary-500"></span>
                 <span className="relative">Create Account</span>
               </button>
-              <div className="mt-8">
+              <div className="mt-6">
                 <span>Already have account?</span>{" "}
                 <span>
-                  <Link to="/signIn" className="text-primary-600 font-semibold hover:underline">
+                  <button onClick={()=>setPath('signIn')} className="text-primary-600 font-semibold hover:underline">
                     Login
-                  </Link>
+                  </button>
                 </span>
               </div>
             </div>
