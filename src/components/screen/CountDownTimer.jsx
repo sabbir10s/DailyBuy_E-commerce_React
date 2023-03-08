9bf0693520bc58de69cc20182364d6b09d6bb8ff
alamincoders <developeralamin03@gmail.com>
2023-03-08 21:48:05 +0600

    updated hero and product card section

--- src/components/screen/CountDownTimer.jsx
+++ src/components/screen/CountDownTimer.jsx
@@ -29,25 +29,25 @@ const CountDownTimer = ({ hoursMinSecs }) => {
           <p className=" bg-white text-primary-500 shadow-custom p-8 lg:p-4 text-xl  h-[23px] md:h-[50px] lg:h-[72px] w-[26px] md:w-[52px] lg:w-[82px] rounded-[6px] lg:rounded-[8px] flex items-center justify-center font-bold text-[10px] md:text-[25px] lg:text-[32px]">
             {days.toString().padStart(2, "0")}
           </p>
-          <p className="text-primary-900 text-[6px] md:text-[14px] lg:text-[18px] mt-2">Day</p>
+          <p className="text-primary-900 text-[14px] lg:text-[18px] mt-2">Day</p>
         </div>
         <div className="text-center inline-block">
           <p className=" bg-white text-primary-500 shadow-custom p-8 lg:p-4 text-xl h-[23px] md:h-[50px] lg:h-[72px] w-[26px] md:w-[52px] lg:w-[82px] rounded-[6px] lg:rounded-[8px] flex items-center justify-center font-bold text-[10px] md:text-[25px] lg:text-[32px]">
             {hrs.toString().padStart(2, "0")}
           </p>
-          <p className="text-primary-900 text-[6px] md:text-[14px] lg:text-[18px] mt-2">Hours</p>
+          <p className="text-primary-900 text-[14px] lg:text-[18px] mt-2">Hours</p>
         </div>
         <div className="text-center inline-block">
           <p className=" bg-white text-primary-500 shadow-custom p-8 lg:p-4 text-xl h-[23px] md:h-[50px] lg:h-[72px] w-[26px] md:w-[52px] lg:w-[82px] rounded-[6px] lg:rounded-[8px] flex items-center justify-center font-bold text-[10px] md:text-[25px] lg:text-[32px]">
             {mins.toString().padStart(2, "0")}
           </p>
-          <p className="text-primary-900 text-[6px] md:text-[14px] lg:text-[18px] mt-2">Minutes</p>
+          <p className="text-primary-900 text-[14px] lg:text-[18px] mt-2">Minutes</p>
         </div>
         <div className="text-center inline-block">
           <p className=" bg-white text-primary-500 shadow-custom p-8 lg:p-4 text-xl h-[23px] md:h-[50px] lg:h-[72px] w-[26px] md:w-[52px] lg:w-[82px] rounded-[6px] lg:rounded-[8px] flex items-center justify-center font-bold text-[10px] md:text-[25px] lg:text-[32px]">
             {secs.toString().padStart(2, "0")}
           </p>
-          <p className="text-primary-900 text-[6px] md:text-[14px] lg:text-[18px] mt-2">Seconds</p>
+          <p className="text-primary-900 text-[14px] lg:text-[18px] mt-2">Seconds</p>
         </div>
       </div>
     </>
