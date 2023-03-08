cae614b78978edcdd2ab6fb030b318f9d7a49275
alamincoders <developeralamin03@gmail.com>
2023-03-08 22:11:22 +0600

    updated

--- src/components/screen/Blog/BlogCard.jsx
+++ src/components/screen/Blog/BlogCard.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const BlogCard = ({blog}) => {
-    const {_id,date, blog_title, img, category} = blog
-    const navigate = useNavigate()
-    const handleBlogDetails = () => {
-        navigate(`/blog/${_id}`)
-    }
-    return (
-        <>
-            <div onClick={()=>handleBlogDetails(_id)} className='border-[1px] border-[#E9E9E9] rounded-[12px] max-w-[400px] cursor-pointer'>
-                <img className='rounded-t-[12px]' src={img} alt="" />
-                <div className='p-[21px]'>
-                    <h1 className='font-bold text-[16px] md:text-[20px] lg:text-[24px] text-left'>{blog_title}</h1>
-                    <div className='my-[12px] h-[1px] bg-[#E9E9E9]'></div>
-                    <div className='flex items-center justify-between text-[12px]'>
-                        <button className='text-primary-600 font-semibold uppercase '>{category}</button>
-                        <p className='text-[#6F757D]'>{date}</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+const BlogCard = ({ blog }) => {
+  const { _id, date, blog_title, img, category } = blog;
+  const navigate = useNavigate();
+  const handleBlogDetails = () => {
+    navigate(`/blog/${_id}`);
+  };
+  return (
+    <>
+      <div
+        onClick={() => handleBlogDetails(_id)}
+        className="border-[1px] border-[#E9E9E9] rounded-[12px] cursor-pointer hover:shadow-custom hover:bg-gray-50 transition ease-in-out duration-300 group"
+      >
+        <div className="w-full h-[250px]">
+          <img loading="lazy" className="rounded-t-[12px] w-full h-full object-fill object-center" src={img} alt="" />
+        </div>
+        <div className="p-[21px]">
+          <h1 className="font-bold text-[16px] md:text-[20px] lg:text-[24px] text-left group-hover:underline group-hover:text-primary-700 duration-300 transition ease-in">
+            {blog_title}
+          </h1>
+          <div className="my-[12px] h-[1px] bg-[#E9E9E9]"></div>
+          <div className="flex items-center justify-between text-[12px]">
+            <button className="text-primary-600 font-semibold uppercase ">{category}</button>
+            <p className="text-[#6F757D]">{date}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BlogCard;
--- src/components/screen/Blog/Blogs.jsx
+++ src/components/screen/Blog/Blogs.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import useBlogs from '../../../hooks/useBlogs';
-import BlogCard from './BlogCard';
+import React from "react";
+import useBlogs from "../../../hooks/useBlogs";
+import BlogCard from "./BlogCard";
 
 const Blogs = () => {
-    const [Blogs] = useBlogs()
-    return (
-        <div className='container mt-[80px] lg:mt-[30px]'>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 justify-center gap-[28px]'>
-                {
-                    Blogs.map(blog=><BlogCard blog={blog} key={blog._id}/>)
-                }
-            </div>
-        </div>
-    );
+  const [Blogs] = useBlogs();
+  return (
+    <div className="container mt-[80px] lg:mt-[30px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {Blogs.map((blog) => (
+          <BlogCard blog={blog} key={blog._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
