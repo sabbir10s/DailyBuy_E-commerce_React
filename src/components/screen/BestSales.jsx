796a413610d671ea44ea542a687b2172350a2fe4
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-09 17:36:27 +0600

    show more show less added

--- src/components/screen/BestSales.jsx
+++ src/components/screen/BestSales.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductCard from "../shared/ProductCard";
 import { FiArrowRight, FiChevronDown } from "react-icons/fi";
+import { BiChevronUp } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const BestSales = () => {
   const [productData, setProductData] = useState([]);
+  const [visible, setVisible] = useState(8)
   const fetchData = () => {
     axios
       .get("/products.json")
@@ -19,6 +21,15 @@ const BestSales = () => {
   useEffect(() => {
     fetchData();
   }, []);
+  const showMoreItem =()=>{
+    setVisible((prevValue)=>prevValue + 4)
+  }
+  const showLessItem =()=>{
+    setVisible((prevValue) =>prevValue - 4)
+  }
+
+  console.log(productData.length);
+  console.log('visible', visible);
   return (
     <>
       <div className=" mt-8 lg:mt-16 container">
@@ -34,19 +45,26 @@ const BestSales = () => {
         </div>
         <div className="container">
           <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[10px] md:gap-[20px]">
-            {productData.map((product, idx) => (
+            {productData.slice(0,visible).map((product, idx) => (
               <ProductCard product={product} key={idx} />
             ))}
           </div>
-          <div className="mt-12 flex justify-center">
-            <Link to="/categories">
-              {" "}
-              <button className="bg-primary-600 hover:bg-primary-500 duration-300 transition hover:shadow-custom hover:scale-110 text-white font-semibold rounded-md uppercase ease-in-out px-[20px] py-[9px] flex items-center gap-1 text-[14px] md:text-[14px]">
-                <span>Shop Now</span>
-                <FiChevronDown className="text-xl" />
-              </button>
-            </Link>
-          </div>
+            {
+              visible < 9 &&  <div className="mt-12 flex justify-center">
+                                    <button onClick={showMoreItem} className="bg-primary-600 hover:bg-primary-500 duration-300 transition hover:shadow-custom hover:scale-110 text-white font-semibold rounded-md uppercase ease-in-out px-[20px] py-[9px] flex items-center gap-1 text-[14px] md:text-[14px]">
+                                      <span>Show More</span>
+                                      <FiChevronDown className="text-xl" />
+                                    </button>
+              </div>
+            }
+            {
+              visible === productData.length &&  <div className="mt-12 flex justify-center">
+                                    <button onClick={showLessItem} className="bg-primary-600 hover:bg-primary-500 duration-300 transition hover:shadow-custom hover:scale-110 text-white font-semibold rounded-md uppercase ease-in-out px-[20px] py-[9px] flex items-center gap-1 text-[14px] md:text-[14px]">
+                                      <span>Show Less</span>
+                                      <BiChevronUp className="text-xl" />
+                                    </button>
+              </div>
+            }
         </div>
       </div>
     </>
