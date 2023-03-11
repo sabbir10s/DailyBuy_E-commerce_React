6fbea5104eda096081157e99f2f43db8cc65681b
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-11 21:53:10 +0600

    my order page done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,8 +6,8 @@ import Password from '../components/userProfile/Password';
 const Profile = () => {
     const [option, setOption] = useState('myAccount')
     return (
-        <div>
-            <div className='flex justify-center mt-[100px] md:mt-[44px] mb-[25px] container'>
+        <div className='mb-[25px] mt-[100px] md:mt-[44px]'>
+            <div className='flex justify-center mb-[25px] container'>
                 <button onClick={()=> setOption('myAccount') } className={option === "myAccount" ? 'border rounded-l-[5px] px-2 md:px-6 py-2 bg-primary-500 text-white border-primary-500' : "border rounded-l-[5px] px-2 md:px-6 py-2 hover:bg-primary-500 hover:text-white hover:border-primary-500"}>MyAccount</button>
                 <button onClick={()=> setOption('myOrders')}className={option === "myOrders" ? 'border px-6 py-2 bg-primary-500 text-white border-primary-500' : "border px-6 py-2 hover:bg-primary-500 hover:text-white hover:border-primary-500"}>My Orders</button>
                 <button onClick={()=> setOption('password')} className={option === "password" ? 'border rounded-r-[5px] px-6 py-2 bg-primary-500 text-white border-primary-500' : "border rounded-r-[5px] px-6 py-2 hover:bg-primary-500 hover:text-white hover:border-primary-500"}>Change Password</button>
