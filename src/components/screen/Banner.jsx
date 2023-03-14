d06619ca8da860e8d7e1cdc9f5cf8cbb49625fbf
sabbir10s <sabbirahmed1023@gmail.com>
2023-03-14 20:16:34 +0600

    text content updated

--- src/components/screen/Banner.jsx
+++ src/components/screen/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
        <div>
        <p className="md:bg-primary-100 md:px-4 md:py-2 rounded text-primary-600 inline-block font-semibold text-[8px] md:text-[12px]">SUMMER SALE UPTO 60%</p>
           <h2 className="mt-[4px] md:mt-[10px] mb-[13px] font-bold text-primary-900 text-[20px] md:text-[25px] lg:text-[32px] leading-[22px] md:leading-[35px] lg:leading-[48px]">Make your life <br /> easy with us</h2>
-          <p className="text-[10px] md:text-[16px] text-gray-500 leading-[15px] md:leading-[24px] md:mb-[30px] lg:mb-[40px]"> Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit</p>
+          <p className="text-[10px] md:text-[16px] text-gray-500 leading-[15px] md:leading-[24px] md:mb-[30px] lg:mb-[40px]"> We offer a wide selection of <br /> high-quality  products at <br /> affordable prices. </p>
           <div className="hidden md:block">
             <Link to="/categories">
               <PrimaryButton title="SHOP NOW" />
